bd95b9d22277b10a5da0345d7546f74459bcaae8
fardosa-ibrahim <99390171+fardosa-ibrahim@users.noreply.github.com>
2022-03-17 18:19:15 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,15 @@
 fun main(){
- var name ="codeHive"
-    var change=name[4].toString()+name[5]+name[6]+name[7]
-    println(change)
+     Name()
     write(5,2)
     sentence("fardosa",22)
-    var word="fardosa"
-    println(word.length)
+    var word=Word()
+    println(word.lengh)
 
+}
+fun Name(){
+    var name="codeHive"
+    var change=name[4].toString()+name[5]+name[6]+name[7]
+    println(change)
 }
 fun write(a:Int, b:Int){
     var number=a%b
@@ -14,4 +17,8 @@ fun write(a:Int, b:Int){
 }
 fun sentence(p:String, z:Int){
     println("Hi my name is $p and i am $z pears old")
+}
+fun Word():String{
+    var words="fardosa"
+    return words
 }
